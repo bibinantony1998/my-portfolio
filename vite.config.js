eb35705e8c39b8b285e95bdb7e5ae86ea029fabe
Bibin Antony <bibinantony1998@gmail.com>
2026-06-21 22:52:25 +0530

    new apps

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { resolve } from 'path';
 
 export default defineConfig({
     base: '/my-portfolio/', // Use relative paths for assets
+    server: {
+        port: 3000,
+    },
     build: {
         rollupOptions: {
             input: {
